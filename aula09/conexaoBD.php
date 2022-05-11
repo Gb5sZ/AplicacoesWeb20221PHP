24bfa4ba55d41b9050831e0ccb41db694f547922
Renan Costa Filgueiras <renan2727@hotmail.com>
2022-05-11 09:37:03 -0300

    Aula10

--- aula09/conexaoBD.php
+++ aula09/conexaoBD.php
@@ -9,7 +9,21 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <title></title>
     </head>
     <body>
+        <form method="POST">
+            Nome <input type="text" name="nome"><br>
+            Cargo <input type="text" name="cargo"><br>
+            Salário Bruto <input type="number" name="salarioB"><br>
+            Desconto INSS <input type="number" name="desconto"><br>
+            <input type="submit" value="Enviar">
+        </form>
         <?php
+             $nome = $_POST["nome"];
+             $cargo = $_POST["cargo"];
+             $salarioB = $_POST["salarioB"];
+             $desconto = $_POST["desconto"];
+             $salarioL = $salarioB-$salarioB*($desconto/100);
+             
+             
              $nome_servidor = "localhost";
              $nome_usuario = "root";
              $senha = "root";
@@ -20,6 +34,26 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                 die("Conexão falhou: " . $conecta->connect_error."<br>");
             }
             echo "Conexão realizada com sucesso <br>";
+            
+            $sql = "INSERT INTO `empresa`.`funcionaio`
+(nome,
+cargo,
+salarioBruto,
+salarioLiquido,
+inss)
+VALUES
+('$nome',
+'$cargo',
+$salarioB,
+$salarioL,
+$desconto);";
+            
+            if ($conecta->multi_query($sql) === TRUE) {
+                echo "Novos registros criados com sucesso<br>";
+            } else {
+                echo "Erro: " . $sql . "<br>" . $conecta->error."<br>";
+            }
+            $conecta->close();
         ?>
     </body>
 </html>
