b14d05bf9df9aaf16fba0b7c7cf4d47d9faef9b5
Renan Costa Filgueiras <renan2727@hotmail.com>
2022-05-25 11:04:14 -0300

    Atualização

--- aula10/resolucaoExercicioComClasse.php
+++ aula10/resolucaoExercicioComClasse.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form method="POST">
-            Nome <input type="text" name="nome"><br>
-            Cargo <input type="text" name="cargo"><br>
-            Salário Bruto <input type="number" name="salarioB"><br>
-            Desconto INSS <input type="number" name="desconto"><br>
-            <input type="submit" value="Inserir" name="inserir">
-            <input type="submit" value="Deletar" name="deletar">
-            <input type="submit" value="Atualizar" name="alterar">
-            <input type="submit" value="Localizar" name="localizar">
-        </form>
-        <?php
-            include 'Funcionario.php';
-            include 'FuncionarioBD.php';
+<?php
+   include 'Funcionario.php';
+   include 'FuncionarioBD.php';
                    
-            $funcionario = new Funcionario(
+   $funcionario = new Funcionario(
                     $_POST["nome"],
                     $_POST["cargo"],
                     $_POST["salarioB"],
@@ -39,6 +18,3 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
             } else {
                 $funcionarioBD->inserir($funcionario);
             }
-        ?>
-    </body>
-</html>
